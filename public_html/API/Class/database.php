d6a8ff848c225a0bbf24345dc1c24547a68913fa
Lucaspers <dunderdykaren@hotmail.com>
2020-04-27 15:02:11 +0200

    enitial commit

---
+++ public_html/API/Class/database.php
@@ -0,0 +1,28 @@
+<?php
+class Database {
+    public $connection;
+    
+    function __construct() {
+
+        $dsn = 'mysql:host=my18b.sqlserver.se;dbname=246400-shoes-us';
+        $user = '246400_at68952';
+        $password = 'grupparbete';
+
+        /*$dsn = 'mysql:host=localhost;dbname=shoes_us';
+        $user = 'root';
+        $password = 'root';*/
+
+        try {
+            $this->connection = new PDO($dsn, $user, $password);
+            $this->connection->exec('set names utf8');
+            error_log("created DB");
+        } catch(PDOException $e) {
+            error_log($e->getMessage());
+            throw $e;
+
+        }
+    }
+}
+
+
+?>
---
+++ public_html/API/recievers/productReciever.php
@@ -0,0 +1,38 @@
+<?php
+
+try {
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        error_log(json_encode($_POST));
+        if($_POST['endpoint'] == 'updateInStock') {
+
+            include('./../Handlers/productHandler.php');
+            $result = updateInStock($_POST["inStock"], $_POST["productID"]);
+            echo json_encode($result); 
+
+        } else {
+            throw new Exception('Not a valid endpoint', 501);
+        }
+
+
+    } else if($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+        if($_GET['endpoint'] == 'getAll') {
+
+            include('./../Handlers/productHandler.php');
+            $result = getAll();
+            echo json_encode($result); 
+
+        } else {
+            throw new Exception('Not a valid endpoint', 501);
+        }
+
+    } else {
+        throw new Exception('Not a valid request method', 405);
+    }
+
+} catch(Exception $e) {
+    echo json_encode(array('Message' => $e->getMessage(), 'status' => $e->getCode()));
+}
+
+?>

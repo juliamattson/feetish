f64fb7acc1640295425adc8f54449cd41ea2ec8f
MichelleLan90 <michelle.lanaro90@gmail.com>
2020-05-03 10:35:25 +0200

    Fixed post metadata 2

--- public_html/wp-content/themes/storefront/template-butiker.php
+++ public_html/wp-content/themes/storefront/template-butiker.php
@@ -30,16 +30,17 @@ get_header(); ?>
 	/* Looping ACF for shops */
 	while ( $loop->have_posts() ) : $loop->the_post();  
 		/* Saving ACF in variables */
-		$shop_name = get_field('butiksnamn');
+		$shop_name = get_the_title();
 		$shop_hours = get_field('oppettider');
 		$shop_adress = get_field('adress');
 		$shop_map = get_field('karta');
-	
 
-		echo '<h1>' . $shop_name . '</h1>';
+
+		echo '<h1>' . '<a href="' . get_the_permalink() . '">' . $shop_name . '</h1>';
 		echo '<p>' . $shop_hours . '</p>';
 		echo '<p>' . $shop_adress . '</p>';
-		echo '<iframe id="shop-map">' . $shop_map . '</iframe>';
+		echo '<iframe id="shop-map" style="width:500px;height:300px;float:right;">' . $shop_map . '</iframe>';
+		echo '<br><br>' . '<hr>';
 
 		// Bug: GoogleMaps inte visas -> API relaterad problem
 		// Hämta post metadata -> Custom Post Type ska visas på single-butik.php sida när man trycker på butiksnamn
@@ -50,7 +51,9 @@ get_header(); ?>
     wp_reset_postdata(); 
  	?>
 
-		
+								
+
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 

fbec242126afb0a2c8b8f4d0770e5ad4291f21cf
MichelleLan90 <michelle.lanaro90@gmail.com>
2020-05-04 10:13:44 +0200

    fixed gogle api map

--- public_html/wp-content/themes/storefront/single-butikerna.php
+++ public_html/wp-content/themes/storefront/single-butikerna.php
@@ -7,12 +7,155 @@
 
 get_header(); ?>
 
+<!-------------------------------------------------------------------------------- KODEN TILL GOOGLE MAP --------------------------------------------------------------------------------------->
+<style type="text/css">
+.acf-map {
+    width: 100%;
+    height: 400px;
+    border: #ccc solid 1px;
+    margin: 20px 0;
+}
+
+
+.acf-map img {
+   max-width: inherit !important;
+}
+</style>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCEOLSxzEjN7kNBZN3qzSL2yl7X_oNmUxw"></script>
+<script type="text/javascript">
+(function( $ ) {
+
+/**
+ * initMap
+ *
+ * Renders a Google Map onto the selected jQuery element
+ *
+ * @date    22/10/19
+ * @since   5.8.6
+ *
+ * @param   jQuery $el The jQuery element.
+ * @return  object The map instance.
+ */
+function initMap( $el ) {
+
+    // Find marker elements within map.
+    var $markers = $el.find('.marker');
+
+    // Create gerenic map.
+    var mapArgs = {
+        zoom        : $el.data('zoom') || 16,
+        mapTypeId   : google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map( $el[0], mapArgs );
+
+    // Add markers.
+    map.markers = [];
+    $markers.each(function(){
+        initMarker( $(this), map );
+    });
+
+    // Center map based on markers.
+    centerMap( map );
+
+    // Return map instance.
+    return map;
+}
+
+/**
+ * initMarker
+ *
+ * Creates a marker for the given jQuery element and map.
+ *
+ * @date    22/10/19
+ * @since   5.8.6
+ *
+ * @param   jQuery $el The jQuery element.
+ * @param   object The map instance.
+ * @return  object The marker instance.
+ */
+function initMarker( $marker, map ) {
+
+    // Get position from marker.
+    var lat = $marker.data('lat');
+    var lng = $marker.data('lng');
+    var latLng = {
+        lat: parseFloat( lat ),
+        lng: parseFloat( lng )
+    };
+
+    // Create marker instance.
+    var marker = new google.maps.Marker({
+        position : latLng,
+        map: map
+    });
+
+    // Append to reference for later use.
+    map.markers.push( marker );
+
+    // If marker contains HTML, add it to an infoWindow.
+    if( $marker.html() ){
+
+        // Create info window.
+        var infowindow = new google.maps.InfoWindow({
+            content: $marker.html()
+        });
+
+        // Show info window when marker is clicked.
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open( map, marker );
+        });
+    }
+}
+
+/**
+ * centerMap
+ *
+ * Centers the map showing all markers in view.
+ *
+ * @date    22/10/19
+ * @since   5.8.6
+ *
+ * @param   object The map instance.
+ * @return  void
+ */
+function centerMap( map ) {
+
+    // Create map boundaries from all map markers.
+    var bounds = new google.maps.LatLngBounds();
+    map.markers.forEach(function( marker ){
+        bounds.extend({
+            lat: marker.position.lat(),
+            lng: marker.position.lng()
+        });
+    });
+
+    // Case: Single marker.
+    if( map.markers.length == 1 ){
+        map.setCenter( bounds.getCenter() );
+
+    // Case: Multiple markers.
+    } else{
+        map.fitBounds( bounds );
+    }
+}
+
+// Render maps on page load.
+$(document).ready(function(){
+    $('.acf-map').each(function(){
+        var map = initMap( $(this) );
+    });
+});
+
+})(jQuery);
+</script>
+<!--------------------------------------------------------------------------------  END OF --------------------------------------------------------------------------------------->
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
 		
-			$shop_name = get_field('butiksnamn');
+		/*	$shop_name = get_field('butiksnamn');
 			$shop_hours = get_field('oppettider');
 			$shop_adress = get_field('adress');
 			$shop_map = get_field('karta');
@@ -21,7 +164,25 @@ get_header(); ?>
 			echo '<p>' . $shop_hours . '</p>';
 			echo '<p>' . $shop_adress . '</p>';
 			echo '<iframe id="shop-map">' . $shop_map . '</iframe>';
-			echo '<br><br>' . '<hr>';
+			echo '<br><br>' . '<hr>';    */
+
+		$shop_name = get_the_title();
+		$shop_hours = get_field('oppettider');
+		$shop_adress = get_field('adress');
+		
+		$location = get_field('karta');
+		if( $location ): ?>
+			<div class="acf-map" data-zoom="16">
+				<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
+			</div>
+		<?php endif;
+
+
+		echo '<p style="text-align:center">' . $shop_adress . '</p>';
+		echo '<h1 style="text-align:center">' . '<a href="' . get_the_permalink() . '">' . $shop_name . '</h1>';
+		echo '<p style="text-align:center">' . $shop_hours . '</p>';
+	//	echo $location;
+		echo '<br><br>' . '<hr>';
 		
 
 		?>
--- public_html/wp-content/themes/storefront/template-butiker.php
+++ public_html/wp-content/themes/storefront/template-butiker.php
@@ -10,38 +10,199 @@
 get_header();
 ?>
 
+<div id="primary" class="content-area">
+<main id="main" class="site-main" role="main">
+
+
+<!-------------------------------------------------------------------------------- KODEN TILL GOOGLE MAP --------------------------------------------------------------------------------------->
+<style type="text/css">
+.acf-map {
+    width: 100%;
+    height: 400px;
+    border: #ccc solid 1px;
+    margin: 20px 0;
+}
+
+
+.acf-map img {
+   max-width: inherit !important;
+}
+</style>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCEOLSxzEjN7kNBZN3qzSL2yl7X_oNmUxw"></script>
+<script type="text/javascript">
+(function( $ ) {
+
+/**
+ * initMap
+ *
+ * Renders a Google Map onto the selected jQuery element
+ *
+ * @date    22/10/19
+ * @since   5.8.6
+ *
+ * @param   jQuery $el The jQuery element.
+ * @return  object The map instance.
+ */
+function initMap( $el ) {
+
+    // Find marker elements within map.
+    var $markers = $el.find('.marker');
+
+    // Create gerenic map.
+    var mapArgs = {
+        zoom        : $el.data('zoom') || 16,
+        mapTypeId   : google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map( $el[0], mapArgs );
+
+    // Add markers.
+    map.markers = [];
+    $markers.each(function(){
+        initMarker( $(this), map );
+    });
+
+    // Center map based on markers.
+    centerMap( map );
+
+    // Return map instance.
+    return map;
+}
+
+/**
+ * initMarker
+ *
+ * Creates a marker for the given jQuery element and map.
+ *
+ * @date    22/10/19
+ * @since   5.8.6
+ *
+ * @param   jQuery $el The jQuery element.
+ * @param   object The map instance.
+ * @return  object The marker instance.
+ */
+function initMarker( $marker, map ) {
+
+    // Get position from marker.
+    var lat = $marker.data('lat');
+    var lng = $marker.data('lng');
+    var latLng = {
+        lat: parseFloat( lat ),
+        lng: parseFloat( lng )
+    };
+
+    // Create marker instance.
+    var marker = new google.maps.Marker({
+        position : latLng,
+        map: map
+    });
+
+    // Append to reference for later use.
+    map.markers.push( marker );
+
+    // If marker contains HTML, add it to an infoWindow.
+    if( $marker.html() ){
+
+        // Create info window.
+        var infowindow = new google.maps.InfoWindow({
+            content: $marker.html()
+        });
+
+        // Show info window when marker is clicked.
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open( map, marker );
+        });
+    }
+}
+
+/**
+ * centerMap
+ *
+ * Centers the map showing all markers in view.
+ *
+ * @date    22/10/19
+ * @since   5.8.6
+ *
+ * @param   object The map instance.
+ * @return  void
+ */
+function centerMap( map ) {
+
+    // Create map boundaries from all map markers.
+    var bounds = new google.maps.LatLngBounds();
+    map.markers.forEach(function( marker ){
+        bounds.extend({
+            lat: marker.position.lat(),
+            lng: marker.position.lng()
+        });
+    });
+
+    // Case: Single marker.
+    if( map.markers.length == 1 ){
+        map.setCenter( bounds.getCenter() );
+
+    // Case: Multiple markers.
+    } else{
+        map.fitBounds( bounds );
+    }
+}
+
+// Render maps on page load.
+$(document).ready(function(){
+    $('.acf-map').each(function(){
+        var map = initMap( $(this) );
+    });
+});
+
+})(jQuery);
+</script>
+<!--------------------------------------------------------------------------------  END OF --------------------------------------------------------------------------------------->
+
+<?php
+	$args = array(  
+	'post_type' => 'butikerna',
+	'post_status' => 'publish',
+	'posts_per_page' => 5, 
+	'orderby' => 'title', 
+	'order' => 'ASC', 
+	);
+
+	$loop = new WP_Query( $args ); 
 
 
 
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	$args = array(  
+	'post_type' => 'butikerna',
+	'post_status' => 'publish',
+	'posts_per_page' => 5, 
+	'orderby' => 'title', 
+	'order' => 'ASC', 
+	);
 
-    <?php
-		$args = array(  
-        'post_type' => 'butikerna',
-        'post_status' => 'publish',
-        'posts_per_page' => 5, 
-        'orderby' => 'title', 
-        'order' => 'ASC', 
-    );
+	$loop = new WP_Query( $args ); 
 
-    $loop = new WP_Query( $args ); 
-		
-	
-	/* Looping ACF for shops */
-	while ( $loop->have_posts() ) : $loop->the_post();  
+
+		/* Looping ACF for shops */
+		while ( $loop->have_posts() ) : $loop->the_post();  
 		/* Saving ACF in variables */
 		$shop_name = get_the_title();
 		$shop_hours = get_field('oppettider');
 		$shop_adress = get_field('adress');
-		$shop_map = get_field('karta');
+		
 
 
 		echo '<h1>' . '<a href="' . get_the_permalink() . '">' . $shop_name . '</h1>';
 		echo '<p>' . $shop_hours . '</p>';
 		echo '<p>' . $shop_adress . '</p>';
-		echo $shop_map;
+
+		$location = get_field('karta');
+		if( $location ): ?>
+			<div class="acf-map" data-zoom="16">
+				<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
+			</div>
+		<?php endif;
+
+
 		echo '<br><br>' . '<hr>';
 
 
@@ -50,8 +211,10 @@ get_header();
     endwhile;
 
     wp_reset_postdata(); 
- 	?>
+?>
 
+
+		
 								
 
 
